95a289cfcdeebdad1253b5db29f78a717f672bcf
Andrey Nikishin <andjix1773@gmail.com>
2023-05-02 12:22:05 +0300

    add code to project

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+// Функция, для которой мы будем искать минимум
+double f(vector<double> x) {
+    return 2 * x[0] * x[0] + 3 * x[1] * x[1] - 4 * x[0] * x[1] + 5 * x[0] - 6 * x[1] + 7;
+}
+
+// Градиент функции
+vector<double> gradient_f(vector<double> x) {
+    vector<double> grad;
+    grad.push_back(4 * x[0] - 4 * x[1] + 5);
+    grad.push_back(6 * x[1] - 4 * x[0] - 6);
+    return grad;
+}
+
+
+// Градиентный спуск
+vector<vector<double>> gradient_descent(vector<double> x_0, double alpha, int num_iterations) {
+    vector<double> x = x_0;
+    vector<vector<double>> massive_x(num_iterations, std::vector<double>(2));
+    for (int i = 0; i < num_iterations; i++) {
+        vector<double> grad = gradient_f(x);
+        for (int j = 0; j < x.size(); j++) {
+            x[j] = x[j] - alpha * grad[j];
+            massive_x[i][j] = x[j];
+        }
+    }
+    return massive_x;
+}
+
+int main() {
+    vector<double> x0 = {8, 8}; // начальное приближение
+    double alpha = 0.15; // скорость обучения
+    int num_iterations = 100; // количество итераций
+    vector<vector<double>> result = gradient_descent(x0, alpha, num_iterations);
+
+    // Запись результатов в файл
+    ofstream fout("result.txt");
+    if (fout.is_open()) {
+        for (int j = 0; j < num_iterations; j++) {
+            fout << result[j][0] << ',';
+            fout << result[j][1] << ',';
+            fout << f(result[j]) << ',';
+            fout << endl;
+        }
+        fout.close();
+    }
+
+    return 0;
+}
---
+++ main.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import matplotlib.animation as animation
+
+os.environ['MPLBACKEND'] = 'tkagg'
+
+x = []
+y = []
+z = []
+
+with open('../../CLionProjects/gradient/cmake-build-debug/result.txt') as f:
+    for line in f:
+        line = line.rstrip(',')  # удаляем запятую в конце строки
+        elements = line.split(',')
+        x.append(float(elements[0]))
+        y.append(float(elements[1]))
+        z.append(float(elements[2]))
+
+def f(x):
+    return 2 * x[0] ** 2 + 3 * x[1] ** 2 - 4 * x[0] * x[1] + 5 * x[0] - 6 * x[1] + 7
+
+N = 100
+xx_0 = [8, 8]
+
+x_plt = np.arange(-10.0, 10.0, 0.1)
+y_plt = np.arange(-10.0, 10.0, 0.1)
+X, Y = np.meshgrid(x_plt, y_plt)
+Z = f([X, Y])
+
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, Z, cmap='coolwarm', alpha=0.8)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.view_init(elev=30, azim=-130)
+point = ax.scatter(xx_0[0], xx_0[1], f(xx_0), c='red')
+
+xx = xx_0
+
+
+def update(num):
+    point._offsets3d = ([x[num]], [y[num]], [z[num]])
+    return point
+
+
+# Создание объекта анимации
+ani = animation.FuncAnimation(fig, update, frames=N, interval=40)
+
+# Сохранение анимации в формате mp4
+ani.save('animation.mp4', writer=animation.FFMpegWriter(fps=20))
